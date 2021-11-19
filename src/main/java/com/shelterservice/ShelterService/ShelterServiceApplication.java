5c1604147901028b0647a5675fa255bd43635e0c
Jakub <bodziov3@gmail.com>
2021-11-19 13:46:25 +0100

    init

---
+++ src/main/java/com/shelterservice/ShelterService/ShelterServiceApplication.java
@@ -0,0 +1,13 @@
+package com.shelterservice.ShelterService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShelterServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShelterServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shelterservice/ShelterService/ShelterServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shelterservice.ShelterService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShelterServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
